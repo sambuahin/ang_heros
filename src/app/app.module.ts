6c482f43ed9497b065138907244bf40b6a2d36ff
samuncc <sbuahin@uncc.edu>
2018-03-22 20:14:25 -0400

    added bootstrap and scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule
